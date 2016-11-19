50d0e2d89e914d015f26c79d201b4557e901de62
Yegie <yegres.vestlam@gmail.com>
2016-11-19 12:05:26 -0500

    added on listers and changed some xmls

--- app/src/main/java/yegie/org/hackathon2016/MainActivity.java
+++ app/src/main/java/yegie/org/hackathon2016/MainActivity.java
@@ -2,6 +2,8 @@ package yegie.org.hackathon2016;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mainButton = (Button) findViewById(R.id.main_button);
+
+        Button settingsButton = (Button) findViewById(R.id.settings_button);
+
+        assert mainButton != null;
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //perform some action
+            }
+        });
+
+        assert settingsButton != null;
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //perform some action
+            }
+        });
     }
+
+
 }
