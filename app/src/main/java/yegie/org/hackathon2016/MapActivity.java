d354fc866998ccd2fa976dae6061b52c539a064d
BuildTools <unconfigured@null.spigotmc.org>
2016-11-19 14:14:14 -0500

    Try to fix issue and add map view

--- app/src/main/java/yegie/org/hackathon2016/MapActivity.java
+++ app/src/main/java/yegie/org/hackathon2016/MapActivity.java
@@ -1,10 +1,21 @@
 package yegie.org.hackathon2016;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Kiera on 11/19/2016.
  */
 
 public class MapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //  requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_map);
+
+    }
 }
